23d3b6dfc95e7aed94e7bcfe539121146988a975
Brylan The Shark <162459810+brylan-the-shark@users.noreply.github.com>
2024-03-20 13:31:36 +0000

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String str = "Hello World";
+    for (int i = 0; i < str.length(); i++) {
+      System.out.println(str.charAt(i));
+    }
   }
 
   // @Test
